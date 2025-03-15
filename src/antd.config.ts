8e7b96c1616e482b3ff463092d0cee5819552da0
kuroshiro1902 <emsinos1902@gmail.com>
2025-03-15 19:37:26 +0700

    feat: config antd color

---
+++ src/antd.config.ts
@@ -0,0 +1,29 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    colorPrimary: 'hsl(199, 89%, 49%)',
+    colorPrimaryText: 'hsl(0, 0%, 100%)',
+    colorBgBase: 'hsl(0 0% 100%)',
+    colorTextBase: 'hsl(222 47% 25%)',
+    colorBgContainer: 'hsl(0, 0%, 98%)',
+    colorText: 'hsl(222, 47%, 11%)',
+    colorBorder: 'hsl(216, 33%, 90%)',
+    colorSuccess: 'hsl(120, 40%, 50%)',
+    colorWarning: 'hsl(39, 100%, 50%)',
+    colorError: 'hsl(0, 100%, 50%)',
+    colorBgElevated: 'hsl(0, 0%, 98%)',
+    colorTextSecondary: 'hsl(215.4, 16.3%, 46.9%)',
+    colorBgSpotlight: 'hsl(216, 33%, 90%)',
+    colorBorderSecondary: 'hsl(216, 33%, 90%)',
+    colorInfo: 'hsl(199, 89%, 49%)',
+    colorBgLayout: 'hsl(0 0% 100%)',
+    colorLink: 'hsl(199, 89%, 49%)',
+    colorLinkHover: 'hsl(199, 89%, 40%)',
+    colorTextPlaceholder: 'hsl(216, 33%, 90%)',
+    colorTextDisabled: 'hsl(215.4, 16.3%, 46.9%)',
+    colorErrorBg: 'hsl(0, 84%, 95%)',
+  },
+};
+
+export default theme;
