8caaf274bf4350b37e63a1a2971af88848ce0903
kuroshiro1902 <emsinos1902@gmail.com>
2025-04-20 18:22:30 +0700

    chore: refactor build setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,32 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import * as path from 'node:path';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './',
-  define: {
-    'process.env': process.env,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => { // mode from vite params
+  const env = loadEnv(mode, process.cwd(), ''); // load env from .env.<mode> file
+  console.log({env});
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    base: './',
+    build: {
+      outDir: 'dist',
     },
-  },
-  server: {
-    port: 5173,
-    // proxy: {
-    //   '/api/v1': {
-    //     // target: 'http://26.30.12.9:9095',
-    //     // target: 'http://26.221.207.60:9095',
-    //     // target: 'http://26.136.15.67:9095',
-    //     changeOrigin: true
-    //   }
-    // }
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
